60c5f5026e4410a26529201d0c319b2524f21090
ADM228 <msh.alexgoog@gmail.com>
2024-01-22 00:09:03 +0300

    start the looping thing

--- main.asm
+++ main.asm
@@ -1,21 +1,41 @@
-;hello.asm
-;  turns on an LED which is connected to PB5 (digital out 13)
+; Soundchip pinout:
+
+;		   __ __
+;		--|  U  |-- GND
+;	CS0	--|		|-- SPI CLK
+;	CS1	--|		|-- SPI DO (MOSI)
+;	GND	--|_____|-- SPI DI (MISO)
+;
+;	The soundchip is the master of SPI
+;	CS0 and CS1 connect to multiplexers to accept 4 targets:
+;		0. DAC (Left)
+;		1. DAC (Right)
+;		2. Incoming register write buffer
+;		3. ROM/RAM with wave data
+;	
+;	The primary DAC im gonna use is DAC7311 by TI. It's smol af
+;	
+
+
+
 
 .include "./tn85def.inc"
+.org 0
+rjmp Init
+.org OVF1addr
+rjmp Cycle
 
-    ldi r16,0
-    mov r1,r16
-    mov r0,r16
+.org INT_VECTORS_SIZE
+Init:
+	ldi r16,0
+	mov r1,r16
+	mov r0,r16
 	ldi r16,1<<PB2
-    mov r17,r16
+	mov r17,r16
 	out DDRB,r16
 	out PortB,r16
-Start:
-    dec r1
-    brne Start
-    dec r0
-    brne Start
-    ; blink
-    eor r16,r17
-    out PortB,r16
-	rjmp Start
+Forever:
+	rjmp Forever
+
+Cycle:
+	reti
