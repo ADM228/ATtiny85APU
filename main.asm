c8a8e06f95b8e0bef632a0de58fd0fefdcd98de2
ADM228 <msh.alexgoog@gmail.com>
2024-01-22 21:01:54 +0300

    INTERRUPTS WORK

--- main.asm
+++ main.asm
@@ -25,6 +25,9 @@
 
 
 .include "./tn85def.inc"
+
+.cseg
+
 .org 0
 rjmp Init
 .org OVF1addr
@@ -33,9 +36,18 @@ rjmp Cycle
 ; .org INT_VECTORS_SIZE	; Unnecessary
 Init:
 	cli
+	.ifdef SPH ; if SPH is defined
+	ldi	r16,	High(RAMEND)
+	out	SPH,	r16 ; Init MSB stack pointer
+	.endif
+	ldi	r16,	Low(RAMEND)
+	out	SPL,	r16 ; Init LSB stack pointer
+	ldi	r20,	(1<<PB3)
 	; Set timer 0 freq to max for SPI + SPI settings
 	clr r16
 	clr r17
+	clr r0
+	clr r1
 	out OCR0A,	r16	; Max frequency
 	out TCCR0B,	r16	; (0<<FOC0A)|(0<<FOC0B)|(0<<WGM02)|(0<<CS0)
 	out USISR,	r16	; (0<<USISIF)|(0<<USIOIF)|(0<<USIPF)|(0<<USICNT0)
@@ -58,46 +70,53 @@ Init:
 	ldi r18,	(1<<PB3)|(1<<PB4)|(0<<PB0)|(1<<PB1)|(1<<PB2)
 	out DDRB,	r18
 	ldi r18,	(1<<PB3)|(1<<PB4)
+	sei
 Forever:
 	rjmp Forever
 
 Cycle:
-	sbis PINB,	PINB0	; If no input pending, skip this
-	rjmp End
-
-	; Enable Timer 0 at max freq
-	out USIDR,	r17	; Clear SPI Data register
-	ldi r16,	(0<<FOC0A)|(0<<FOC0B)|(0<<WGM02)|(1<<USICS0)
-	out TCCR0B,	r16
-	; Wait, somehow
-WaitLoop:
-	in	r16,	USISR
-	sbrs r16,	USIOIF
-	rjmp WaitLoop
-	
-	out	TCCR0B,	r17		; Disable clock
-	
-	; Get output
-	in	r16,	USIDR
-	cpi r16,	1	; Check address
+	dec r0
 	brne End
 
-	; The address is right
-	; Enable Timer 0 at max freq
-	out USIDR,	r17	; Clear SPI Data register
-	ldi r16,	(0<<FOC0A)|(0<<FOC0B)|(0<<WGM02)|(1<<USICS0)
-	out TCCR0B,	r16
-	; Wait, somehow
-WaitLoop2:
-	in	r16,	USISR
-	sbrs r16,	USIOIF
-	rjmp WaitLoop2
+	in 	r19,	PortB
+	eor	r19,	r20
+	out	PortB,	r19
+; 	sbis PINB,	PINB0	; If no input pending, skip this
+; 	rjmp End
 
-	out	TCCR0B,	r17		; Disable clock
-	; Get output, Toggle LED
-	in	r16,	USIDR
-	and	r16,	r18
-	out	PortB,	r16
+; 	; Enable Timer 0 at max freq
+; 	out USIDR,	r17	; Clear SPI Data register
+; 	ldi r16,	(0<<FOC0A)|(0<<FOC0B)|(0<<WGM02)|(1<<USICS0)
+; 	out TCCR0B,	r16
+; 	; Wait, somehow
+; WaitLoop:
+; 	in	r16,	USISR
+; 	sbrs r16,	USIOIF
+; 	rjmp WaitLoop
+	
+; 	out	TCCR0B,	r17		; Disable clock
+	
+; 	; Get output
+; 	in	r16,	USIDR
+; 	cpi r16,	1	; Check address
+; 	brne End
+
+; 	; The address is right
+; 	; Enable Timer 0 at max freq
+; 	out USIDR,	r17	; Clear SPI Data register
+; 	ldi r16,	(0<<FOC0A)|(0<<FOC0B)|(0<<WGM02)|(1<<USICS0)
+; 	out TCCR0B,	r16
+; 	; Wait, somehow
+; WaitLoop2:
+; 	in	r16,	USISR
+; 	sbrs r16,	USIOIF
+; 	rjmp WaitLoop2
+
+; 	out	TCCR0B,	r17		; Disable clock
+; 	; Get output, Toggle LED
+; 	in	r16,	USIDR
+; 	and	r16,	r18
+; 	out	PortB,	r16
 
 End:
 	reti
--- makefile
+++ makefile
@@ -1,11 +1,16 @@
 
 # Only intended for linux.
 build: main.hex
-main.hex: main.asm
-	avra main.asm
+debug: main.elf
+
+main.hex main.map: main.asm
+	avra -m main.map main.asm
+
+main.elf: main.hex main.map
+	avr-objcopy -v -I ihex -O elf32-avr main.hex main.elf
 
 flash: main.hex
 	avrdude -v -p attiny85 -c stk500v1 -b19200 -P /dev/ttyUSB0 -U flash:w:main.hex:i
 
 
-.PHONY: build flash
+.PHONY: build flash debug
